a7e1ba026e997f4e166dd53d3aae67e9da53782a
Sam <burtman.samuel26@gmail.com>
2023-09-30 21:35:31 +0300

    update src/gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
 const { program } = require('commander');
 
 program
@@ -6,25 +8,23 @@ program
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    const absolutePath1 = path.resolve(filepath1);
+    const absolutePath2 = path.resolve(filepath2);
+
+    const ext1 = path.extname(absolutePath1);
+    const ext2 = path.extname(absolutePath2);
+
+    let data1, data2;
+    if (ext1 === '.json' && ext2 === '.json') {
+      data1 = JSON.parse(fs.readFileSync(absolutePath1, 'utf-8'));
+      data2 = JSON.parse(fs.readFileSync(absolutePath2, 'utf-8'));
+    } else {
+      console.error('Error: Unsupported file format.');
+      return;
+    }
+
+    console.log('Parsed Data 1:', data1);
+    console.log('Parsed Data 2:', data2);
+  })
   .parse(process.argv);
-
-program
-  .on('--help', () => {
-    console.log(`
-  Examples:
-    gendiff file1.json file2.json -f plain
-  `);
-  });
-
-const filePath1 = program.args[0];
-const filePath2 = program.args[1];
-const format = program.format;
-
-if (!filePath1 || !filePath2) {
-  console.error('Error: Please provide both file paths.');
-  program.help();
-}
-
-console.log('File Path 1:', filePath1);
-console.log('File Path 2:', filePath2);
-console.log('Format:', format);
