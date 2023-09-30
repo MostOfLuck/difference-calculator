1d2f6c2924cc1481ed5ccb9c11ac6101ee62178d
Sam <burtman.samuel26@gmail.com>
2023-09-30 20:38:37 +0300

    update src/gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,11 +2,29 @@
 const { program } = require('commander');
 
 program
-  .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-h, --help', 'output usage information')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
   .parse(process.argv);
 
-if (program.help) {
+program
+  .on('--help', () => {
+    console.log(`
+  Examples:
+    gendiff file1.json file2.json -f plain
+  `);
+  });
+
+const filePath1 = program.args[0];
+const filePath2 = program.args[1];
+const format = program.format;
+
+if (!filePath1 || !filePath2) {
+  console.error('Error: Please provide both file paths.');
   program.help();
 }
+
+console.log('File Path 1:', filePath1);
+console.log('File Path 2:', filePath2);
+console.log('Format:', format);
