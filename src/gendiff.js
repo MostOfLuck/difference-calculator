a436ed884913e7db9a1205cd56e1e6ab581447d7
Sam <burtman.samuel26@gmail.com>
2023-09-30 21:41:02 +0300

    update gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,6 +2,37 @@
 const fs = require('fs');
 const path = require('path');
 const { program } = require('commander');
+const _ = require('lodash');
+
+function genDiff(filepath1, filepath2) {
+  const absolutePath1 = path.resolve(filepath1);
+  const absolutePath2 = path.resolve(filepath2);
+
+  const data1 = JSON.parse(fs.readFileSync(absolutePath1, 'utf-8'));
+  const data2 = JSON.parse(fs.readFileSync(absolutePath2, 'utf-8'));
+
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+
+  const addedKeys = _.difference(keys2, keys1);
+  const removedKeys = _.difference(keys1, keys2);
+  const commonKeys = _.intersection(keys1, keys2);
+
+  const diff = commonKeys.map((key) => {
+    if (data1[key] === data2[key]) {
+      return `  ${key}: ${data1[key]}`;
+    }
+    return `- ${key}: ${data1[key]}\n+ ${key}: ${data2[key]}`;
+  });
+
+  const result = [
+    ...addedKeys.map((key) => `+ ${key}: ${data2[key]}`),
+    ...removedKeys.map((key) => `- ${key}: ${data1[key]}`),
+    ...diff,
+  ];
+
+  return `{\n${result.join('\n')}\n}`;
+}
 
 program
   .version('1.0.0')
@@ -9,22 +40,7 @@ program
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format')
   .action((filepath1, filepath2) => {
-    const absolutePath1 = path.resolve(filepath1);
-    const absolutePath2 = path.resolve(filepath2);
-
-    const ext1 = path.extname(absolutePath1);
-    const ext2 = path.extname(absolutePath2);
-
-    let data1, data2;
-    if (ext1 === '.json' && ext2 === '.json') {
-      data1 = JSON.parse(fs.readFileSync(absolutePath1, 'utf-8'));
-      data2 = JSON.parse(fs.readFileSync(absolutePath2, 'utf-8'));
-    } else {
-      console.error('Error: Unsupported file format.');
-      return;
-    }
-
-    console.log('Parsed Data 1:', data1);
-    console.log('Parsed Data 2:', data2);
+    const diff = genDiff(filepath1, filepath2);
+    console.log(diff);
   })
   .parse(process.argv);
