0dfc19e472f40eedbcba753b7265f2e0b158d9a6
Sam <burtman.samuel26@gmail.com>
2023-10-26 17:46:02 +0200

    update tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,36 +9,42 @@ const yamlFile2Path = path.join(__dirname, '__fixtures__/file2.yml');
 
 test('Compare flat JSON files (stylish format)', () => {
   const expected = readFileSync('__tests__/__fixtures__/expected_stylish.txt', 'utf-8');
+  process.argv = ['', '', jsonFile1Path, jsonFile2Path];
   const diff = genDiff(jsonFile1Path, jsonFile2Path, 'stylish');
   expect(diff).toBe(expected);
 });
 
 test('Compare flat JSON files (plain format)', () => {
   const expected = readFileSync('__tests__/__fixtures__/expected_plain.txt', 'utf-8');
+  process.argv = ['', '', jsonFile1Path, jsonFile2Path, '-f', 'plain'];
   const diff = genDiff(jsonFile1Path, jsonFile2Path, 'plain');
   expect(diff).toBe(expected);
 });
 
 test('Compare flat JSON files (json format)', () => {
   const expected = readFileSync('__tests__/__fixtures__/expected_json.json', 'utf-8');
+  process.argv = ['', '', jsonFile1Path, jsonFile2Path, '-f', 'json'];
   const diff = genDiff(jsonFile1Path, jsonFile2Path, 'json');
   expect(JSON.parse(diff)).toEqual(JSON.parse(expected));
 });
 
 test('Compare flat YAML files (stylish format)', () => {
   const expected = readFileSync('__tests__/__fixtures__/expected_stylish.txt', 'utf-8');
+  process.argv = ['', '', yamlFile1Path, yamlFile2Path];
   const diff = genDiff(yamlFile1Path, yamlFile2Path, 'stylish');
   expect(diff).toBe(expected);
 });
 
 test('Compare flat YAML files (plain format)', () => {
   const expected = readFileSync('__tests__/__fixtures__/expected_plain.txt', 'utf-8');
+  process.argv = ['', '', yamlFile1Path, yamlFile2Path, '-f', 'plain'];
   const diff = genDiff(yamlFile1Path, yamlFile2Path, 'plain');
   expect(diff).toBe(expected);
 });
 
 test('Compare flat YAML files (json format)', () => {
   const expected = readFileSync('__tests__/__fixtures__/expected_json.json', 'utf-8');
+  process.argv = ['', '', yamlFile1Path, yamlFile2Path, '-f', 'json'];
   const diff = genDiff(yamlFile1Path, yamlFile2Path, 'json');
   expect(JSON.parse(diff)).toEqual(JSON.parse(expected));
 });
