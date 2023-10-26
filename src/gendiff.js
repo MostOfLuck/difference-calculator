1f398baee1048efb19a0a1a665c4a2f0fd740d5b
Sam <burtman.samuel26@gmail.com>
2023-10-26 18:33:13 +0200

    update gendiff

--- src/gendiff.js
+++ src/gendiff.js
@@ -86,6 +86,31 @@ function generatePlainDiff(data1, data2) {
   return result.join('\n');
 }
 
+function generateJsonDiff(data1, data2) {
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+
+  const addedKeys = keys2.filter((key) => !keys1.includes(key));
+  const removedKeys = keys1.filter((key) => !keys2.includes(key));
+  const updatedKeys = keys1.filter((key) => keys2.includes(key) && data1[key] !== data2[key]);
+
+  const result = {};
+
+  addedKeys.forEach((key) => {
+    result[key] = { type: 'added', value: data2[key] };
+  });
+
+  removedKeys.forEach((key) => {
+    result[key] = { type: 'removed', value: data1[key] };
+  });
+
+  updatedKeys.forEach((key) => {
+    result[key] = { type: 'updated', oldValue: data1[key], newValue: data2[key] };
+  });
+
+  return result;
+}
+
 const program = new Command();
 
 program
@@ -115,7 +140,10 @@ program
 
     const { format } = program.opts();
 
-    if (format === 'plain') {
+    if (format === 'json') {
+      const diff = generateJsonDiff(data1, data2);
+      console.log(JSON.stringify(diff, null, 2));
+    } else if (format === 'plain') {
       const diff = generatePlainDiff(data1, data2);
       console.log(diff);
     } else {
