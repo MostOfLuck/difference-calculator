7549f6e3f3a6a482279265f3e6e6e77e50485ed2
Sam <burtman.samuel26@gmail.com>
2023-11-20 20:28:33 +0100

    update src/gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,50 +1,61 @@
 #!/usr/bin/env node
-import { readFileSync } from 'fs';
+import fs from 'fs';
+import path from 'path';
 import jsYaml from 'js-yaml';
-import { formatValue } from '../formatters/index.js';
+import formatValue from '../formatters/index.js';
 
-export function parseJsonFile(filePath) {
-  const fileContent = readFileSync(filePath, 'utf-8');
-  return JSON.parse(fileContent);
+export function parseFile(file) {
+  const fileExt = path.extname(file).toLowerCase();
+  const fileContent = fs.readFileSync(file, 'utf-8');
+
+  switch (fileExt) {
+    case '.json':
+      return JSON.parse(fileContent);
+    case '.yml':
+    case '.yaml':
+      return parseYaml(fileContent);
+    default:
+      throw new Error(`Unsupported file format: ${fileExt}`);
+  }
 }
 
-export function parseYamlFile(filePath) {
-  const fileContent = readFileSync(filePath, 'utf-8');
-  return jsYaml.load(fileContent);
+export function parseYaml(yamlString) {
+  try {
+    return jsYaml.load(yamlString);
+  } catch (error) {
+    throw new Error(`Error while parsing YAML: ${error.message}`);
+  }
 }
 
-export function generateDiff(data1, data2, depth = 1) {
+export function generateDiff(data1, data2) {
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
 
   const addedKeys = keys2.filter((key) => !keys1.includes(key));
   const removedKeys = keys1.filter((key) => !keys2.includes(key));
-  const commonKeys = keys1.filter((key) => keys2.includes(key));
+  const updatedKeys = keys1.filter((key) => keys2.includes(key) && data1[key] !== data2[key]);
 
-  const diff = commonKeys.map((key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
+  const result = {};
 
-    if (typeof value1 === 'object' && typeof value2 === 'object') {
-      const nestedDiff = generateDiff(value1, value2, depth + 1);
-      return `    ${'  '.repeat(depth)}${key}: ${nestedDiff}`;
-    }
-
-    if (value1 === value2) {
-      return `    ${'  '.repeat(depth)}${key}: ${value1}`;
-    }
-
-    return [
-      `${'  '.repeat(depth)}- ${key}: ${value1}`,
-      `${'  '.repeat(depth)}+ ${key}: ${value2}`,
-    ].join('\n');
+  addedKeys.forEach((key) => {
+    result[key] = { type: 'added', value: data2[key] };
   });
 
-  const result = [
-    ...addedKeys.map((key) => `  + ${'  '.repeat(depth)}${key}: ${formatValue(data2[key], depth)}`),
-    ...removedKeys.map((key) => `  - ${'  '.repeat(depth)}${key}: ${formatValue(data1[key], depth)}`),
-    ...diff,
-  ];
+  removedKeys.forEach((key) => {
+    result[key] = { type: 'removed', value: data1[key] };
+  });
 
-  return `{\n${result.join('\n')}\n${'  '.repeat(depth - 1)}}`;
+  updatedKeys.forEach((key) => {
+    result[key] = { type: 'updated', oldValue: data1[key], newValue: data2[key] };
+  });
+
+  return result;
+}
+
+export default function generateFormattedDiff(file1, file2, format = 'stylish') {
+  const data1 = parseFile(file1);
+  const data2 = parseFile(file2);
+  const diff = generateDiff(data1, data2);
+
+  return formatValue(diff, format);
 }
