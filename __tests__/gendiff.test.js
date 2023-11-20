68b3174589452fc106901ae1b0c9e6cd175324e0
Sam <burtman.samuel26@gmail.com>
2023-11-20 20:27:22 +0100

    update __tests__/gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,57 +1,55 @@
+// __tests__/gendiff.test.js
 import { readFileSync } from 'fs';
 import path from 'path';
-import jsYaml from 'js-yaml';
-import generateDiff from '../src/gendiff.js';
+import { generateDiff } from '../src/gendiff.js';
 
-const jsonFile1Path = path.join(__dirname, '__fixtures__/file1.json');
-const jsonFile2Path = path.join(__dirname, '__fixtures__/file2.json');
-const yamlFile1Path = path.join(__dirname, '__fixtures__/file1.yml');
-const yamlFile2Path = path.join(__dirname, '__fixtures__/file2.yml');
+const baseDir = process.cwd();
+const getFixturePath = (filename) => path.join(baseDir, '__tests__/__fixtures__', filename);
 
-test('Compare flat JSON files (stylish format)', () => {
-  const expected = readFileSync('__tests__/__fixtures__/expected_stylish.txt', 'utf-8');
-  const data1 = JSON.parse(readFileSync(jsonFile1Path, 'utf-8'));
-  const data2 = JSON.parse(readFileSync(jsonFile2Path, 'utf-8'));
+test('Compare flat JSON files (stylish format)', async () => {
+  const expected = await readFileSync(getFixturePath('expected_stylish.txt'), 'utf-8');
+  const data1 = await readFileSync(getFixturePath('file1.json'), 'utf-8');
+  const data2 = await readFileSync(getFixturePath('file2.json'), 'utf-8');
   const diff = generateDiff(data1, data2, 'stylish');
   expect(diff).toBe(expected);
 });
 
-test('Compare flat JSON files (plain format)', () => {
-  const expected = readFileSync('__tests__/__fixtures__/expected_plain.txt', 'utf-8');
-  const data1 = JSON.parse(readFileSync(jsonFile1Path, 'utf-8'));
-  const data2 = JSON.parse(readFileSync(jsonFile2Path, 'utf-8'));
+test('Compare flat JSON files (plain format)', async () => {
+  const expected = await readFileSync(getFixturePath('expected_plain.txt'), 'utf-8');
+  const data1 = await readFileSync(getFixturePath('file1.json'), 'utf-8');
+  const data2 = await readFileSync(getFixturePath('file2.json'), 'utf-8');
   const diff = generateDiff(data1, data2, 'plain');
   expect(diff).toBe(expected);
 });
 
-test('Compare flat JSON files (json format)', () => {
-  const expected = JSON.parse(readFileSync('__tests__/__fixtures__/expected_json.json', 'utf-8'));
-  const data1 = JSON.parse(readFileSync(jsonFile1Path, 'utf-8'));
-  const data2 = JSON.parse(readFileSync(jsonFile2Path, 'utf-8'));
+test('Compare flat JSON (json format)', async () => {
+  const expected = JSON.parse(await readFileSync(getFixturePath('expected_json.json'), 'utf-8'));
+  const data1 = await readFileSync(getFixturePath('file1.json'), 'utf-8');
+  const data2 = await readFileSync(getFixturePath('file2.json'), 'utf-8');
   const diff = generateDiff(data1, data2, 'json');
   expect(diff).toEqual(expected);
 });
 
-test('Compare flat YAML files (stylish format)', () => {
-  const expected = readFileSync('__tests__/__fixtures__/expected_stylish.txt', 'utf-8');
-  const data1 = jsYaml.load(readFileSync(yamlFile1Path, 'utf-8'));
-  const data2 = jsYaml.load(readFileSync(yamlFile2Path, 'utf-8'));
+test('Compare flat YAML files (stylish format)', async () => {
+  const expected = await readFileSync(getFixturePath('expected_stylish.txt'), 'utf-8');
+  const data1 = await readFileSync(getFixturePath('file1.yml'), 'utf-8');
+  const data2 = await readFileSync(getFixturePath('file2.yml'), 'utf-8');
   const diff = generateDiff(data1, data2, 'stylish');
   expect(diff).toBe(expected);
 });
 
-test('Compare flat YAML files (plain format)', () => {
-  const expected = readFileSync('__tests__/__fixtures__/expected_plain.txt', 'utf-8');
-  const data1 = jsYaml.load(readFileSync(yamlFile1Path, 'utf-8'));
-  const data2 = jsYaml.load(readFileSync(yamlFile2Path, 'utf-8'));
+test('Compare flat YAML files (plain format)', async () => {
+  const expected = await readFileSync(getFixturePath('expected_plain.txt'), 'utf-8');
+  const data1 = await readFileSync(getFixturePath('file1.yml'), 'utf-8');
+  const data2 = await readFileSync(getFixturePath('file2.yml'), 'utf-8');
   const diff = generateDiff(data1, data2, 'plain');
   expect(diff).toBe(expected);
 });
 
-test('Compare flat YAML files (json format)', () => {
-  const expected = JSON.parse(readFileSync('__tests__/__fixtures__/expected_json.json', 'utf-8'));
-  const data1 = jsYaml.load(readFileSync(yamlFile1Path, 'utf-8'));
-  const data2 = jsYaml.load(readFileSync(yamlFile2Path, 'utf-8'));
+test('Compare flat YAML files (json format)', async () => {
+  const expected = JSON.parse(await readFileSync(getFixturePath('expected_json.json'), 'utf-8'));
+  const data1 = await readFileSync(getFixturePath('file1.yml'), 'utf-8');
+  const data2 = await readFileSync(getFixturePath('file2.yml'), 'utf-8');
   const diff = generateDiff(data1, data2, 'json');
   expect(diff).toEqual(expected);
 });
