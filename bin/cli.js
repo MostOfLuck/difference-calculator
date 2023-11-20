f48e482914660e77829f06be88bad5b6b659973b
Sam <burtman.samuel26@gmail.com>
2023-11-20 20:27:36 +0100

    update bin/cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { parseJsonFile, parseYamlFile, generateDiff } from '../src/gendiff.js';
-import { generateJsonDiff, generatePlainDiff } from '../formatters/index.js';
+import path from 'path';
+import generateFormattedDiff from '../src/gendiff.js';
 
 const program = new Command();
 
@@ -9,38 +9,20 @@ program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format (json, plain)')
-  .action((filepath1, filepath2) => {
-    let data1;
-    let data2;
+  .option('-f, --format <type>', 'output format (stylish, plain, json)')
+  .action(async (filepath1, filepath2, options) => {
+    try {
+      const { format } = options;
 
-    if (filepath1.endsWith('.json')) {
-      data1 = parseJsonFile(filepath1);
-    } else if (filepath1.endsWith('.yaml') || filepath1.endsWith('.yml')) {
-      data1 = parseYamlFile(filepath1);
-    } else {
-      throw new Error(`Unsupported file format: ${filepath1}`);
-    }
+      // Resolve the absolute paths based on the current working directory
+      const resolvedPath1 = path.resolve(process.cwd(), '__tests__/__fixtures__', filepath1);
+      const resolvedPath2 = path.resolve(process.cwd(), '__tests__/__fixtures__', filepath2);
 
-    if (filepath2.endsWith('.json')) {
-      data2 = parseJsonFile(filepath2);
-    } else if (filepath2.endsWith('.yaml') || filepath2.endsWith('.yml')) {
-      data2 = parseYamlFile(filepath2);
-    } else {
-      throw new Error(`Unsupported file format: ${filepath2}`);
-    }
-
-    const { format } = program.opts();
-
-    if (format === 'json') {
-      const diff = generateJsonDiff(data1, data2);
-      console.log(JSON.stringify(diff, null, 2));
-    } else if (format === 'plain') {
-      const diff = generatePlainDiff(data1, data2);
+      const diff = await generateFormattedDiff(resolvedPath1, resolvedPath2, format);
       console.log(diff);
-    } else {
-      const diff = generateDiff(data1, data2);
-      console.log(diff);
+    } catch (error) {
+      console.error(`Error reading or parsing file: ${error.message}`);
+      process.exit(1);
     }
   });
 
