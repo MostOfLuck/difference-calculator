9744b0de38bfa693339dfbce9da1bf2fe2c3c306
Sam <burtman.samuel26@gmail.com>
2023-11-20 20:36:06 +0100

    update cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -9,16 +9,15 @@ program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format (stylish, plain, json)')
+  .option('-f, --format <type>', 'output format (stylish, plain, json)', 'stylish')
   .action(async (filepath1, filepath2, options) => {
     try {
       const { format } = options;
 
-      // Resolve the absolute paths based on the current working directory
       const resolvedPath1 = path.resolve(process.cwd(), '__tests__/__fixtures__', filepath1);
       const resolvedPath2 = path.resolve(process.cwd(), '__tests__/__fixtures__', filepath2);
 
-      const diff = await generateFormattedDiff(resolvedPath1, resolvedPath2, format);
+      const diff = await generateFormattedDiff(resolvedPath1, resolvedPath2, { format });
       console.log(diff);
     } catch (error) {
       console.error(`Error reading or parsing file: ${error.message}`);
