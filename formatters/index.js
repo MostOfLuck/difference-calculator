250c02a17cabe1d0a0649da6d055f5bd3630bcf6
Sam <burtman.samuel26@gmail.com>
2023-11-28 10:51:29 +0100

    update formatters/index.js

--- formatters/index.js
+++ formatters/index.js
@@ -4,14 +4,7 @@ export default function formatValue(value, depth) {
     const keys = Object.keys(value);
     const formattedLines = keys.map((key) => {
       const formattedValue = formatValue(value[key], depth + 1);
-      const type = value[key]?.type;
-      let sign = ' ';
-      if (type === 'added') {
-        sign = '+';
-      } else if (type === 'removed') {
-        sign = '-';
-      }
-      return `${' '.repeat(depth * 4)}${sign} ${key}: ${formattedValue}`;
+      return `${' '.repeat(depth * 4)}  ${key}: ${formattedValue}`;
     });
     return `{\n${formattedLines.join('\n')}\n${' '.repeat((depth - 1) * 4)}}`;
   }
