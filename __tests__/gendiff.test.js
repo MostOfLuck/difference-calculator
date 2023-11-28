cd3f944c2208097b6392004990b06cb106bb0e90
Sam <burtman.samuel26@gmail.com>
2023-11-28 15:43:18 +0100

    update __tests__/gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,10 +1,18 @@
 // __tests__/gendiff.test.js
+import { execSync } from 'child_process';
 import { readFileSync } from 'fs';
 import path from 'path';
 import { generateDiff } from '../src/gendiff.js';
 
 const baseDir = process.cwd();
 const getFixturePath = (filename) => path.join(baseDir, '__tests__/__fixtures__', filename);
+
+const cliPath = path.resolve(__dirname, '..', 'bin', 'cli.js');
+
+test('CLI tool produces correct output with stylish format', () => {
+  const result = execSync(`node ${cliPath} file1.json file2.json -f stylish`).toString();
+  expect(result).toContain('expected output');
+});
 
 test('Compare flat JSON files (stylish format)', async () => {
   const expected = await readFileSync(getFixturePath('expected_stylish.txt'), 'utf-8');
