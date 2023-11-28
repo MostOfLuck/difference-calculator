fafabfaa6b1c5552524ceaa4a9e5d6e32d517f53
Sam <burtman.samuel26@gmail.com>
2023-11-28 12:28:45 +0100

    update bin/cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import path from 'path';
-import generateFormattedDiff from '../src/gendiff.js';
+import { parseYamlFile, generateDiff, parseJsonFile } from '../src/gendiff.js';
+import generatePlainDiff from '../formatters/plainFormatter.js';
 
 const program = new Command();
 
@@ -9,19 +10,40 @@ program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format (stylish, plain, json)', 'stylish')
-  .action(async (filepath1, filepath2, options) => {
-    try {
-      const { format } = options;
+  .option('-f, --format <type>', 'output format (json, plain)')
+  .action((filepath1, filepath2) => {
+    // Assuming the '__tests__/__fixtures__' directory structure
+    const fixturesPath = path.resolve(process.cwd(), '__tests__/__fixtures__');
+    const fullPath1 = path.join(fixturesPath, filepath1);
+    const fullPath2 = path.join(fixturesPath, filepath2);
 
-      const resolvedPath1 = path.resolve(process.cwd(), '__tests__/__fixtures__', filepath1);
-      const resolvedPath2 = path.resolve(process.cwd(), '__tests__/__fixtures__', filepath2);
+    let data1;
+    let data2;
 
-      const diff = await generateFormattedDiff(resolvedPath1, resolvedPath2, { format });
+    if (fullPath1.endsWith('.json')) {
+      data1 = parseJsonFile(fullPath1);
+    } else if (fullPath1.endsWith('.yaml') || fullPath1.endsWith('.yml')) {
+      data1 = parseYamlFile(fullPath1);
+    } else {
+      throw new Error(`Unsupported file format: ${fullPath1}`);
+    }
+
+    if (fullPath2.endsWith('.json')) {
+      data2 = parseJsonFile(fullPath2);
+    } else if (fullPath2.endsWith('.yaml') || fullPath2.endsWith('.yml')) {
+      data2 = parseYamlFile(fullPath2);
+    } else {
+      throw new Error(`Unsupported file format: ${fullPath2}`);
+    }
+
+    const { format } = program.opts();
+
+    if (format === 'plain') {
+      const diff = generatePlainDiff(data1, data2);
       console.log(diff);
-    } catch (error) {
-      console.error(`Error reading or parsing file: ${error.message}`);
-      process.exit(1);
+    } else {
+      const diff = generateDiff(data1, data2);
+      console.log(diff);
     }
   });
 
