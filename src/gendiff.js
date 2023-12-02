b91b6244865220f8301e67f7b24ad843533fed54
Sam <burtman.samuel26@gmail.com>
2023-12-02 13:33:21 +0100

    update gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -12,18 +12,6 @@ export function parseYamlFile(filePath) {
   return jsYaml.load(fileContent);
 }
 
-export function formatValue(value, depth) {
-  if (typeof value === 'object' && value !== null) {
-    const keys = Object.keys(value);
-    const formattedLines = keys.map((key) => {
-      const formattedValue = formatValue(value[key], depth + 1);
-      return `${' '.repeat(depth * 4)}  ${key}: ${formattedValue}`;
-    });
-    return `{\n${formattedLines.join('\n')}\n${' '.repeat((depth - 1) * 4)}}`;
-  }
-  return value;
-}
-
 export function generateDiff(data1, data2, depth = 1) {
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
@@ -58,4 +46,41 @@ export function generateDiff(data1, data2, depth = 1) {
   ];
 
   return `{\n${result.join('\n')}\n${'  '.repeat(depth - 1)}}`;
+}
+
+export function formatValue(value, depth) {
+  if (typeof value === 'object' && value !== null) {
+    const keys = Object.keys(value);
+    const formattedLines = keys.map((key) => {
+      const formattedValue = formatValue(value[key], depth + 1);
+      return `${' '.repeat(depth * 4)}  ${key}: ${formattedValue}`;
+    });
+    return `{\n${formattedLines.join('\n')}\n${' '.repeat((depth - 1) * 4)}}`;
+  }
+  return value;
+}
+
+export function generatePlainDiff(data1, data2) {
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+
+  const addedKeys = keys2.filter((key) => !keys1.includes(key));
+  const removedKeys = keys1.filter((key) => !keys2.includes(key));
+  const updatedKeys = keys1.filter((key) => keys2.includes(key) && data1[key] !== data2[key]);
+
+  const result = [];
+
+  addedKeys.forEach((key) => {
+    result.push(`Property '${key}' was added with value: ${formatValue(data2[key])}`);
+  });
+
+  removedKeys.forEach((key) => {
+    result.push(`Property '${key}' was removed`);
+  });
+
+  updatedKeys.forEach((key) => {
+    result.push(`Property '${key}' was updated. From ${formatValue(data1[key])} to ${formatValue(data2[key])}`);
+  });
+
+  return result.join('\n');
 }
