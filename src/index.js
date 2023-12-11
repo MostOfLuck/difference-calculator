5e243e9ae018212c979c6fc3404359c6d5e848e4
Sam <burtman.samuel26@gmail.com>
2023-12-11 19:40:01 +0100

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,28 @@ import compare from './compare.js';
 import parse from './parsers.js';
 import ast from './formatters/index.js';
 
-const getData = (filePath) => {
-  const fileContent = fs.readFileSync(filePath, 'utf-8').trim();
-  const fileExtName = path.extname(filePath).slice(1);
-  return parse(fileContent, fileExtName);
+const readFile = (filePath) => {
+  const fullPath = path.resolve(process.cwd(), filePath);
+  const data = fs.readFileSync(fullPath, 'utf-8').trim();
+  return data;
 };
 
-const getDiff = (file1, file2, format = 'stylish') => ast(compare(getData(file1), getData(file2)), format);
+const readFiles = (path1, path2) => {
+  const data1 = readFile(path1);
+  const data2 = readFile(path2);
+  return [data1, data2];
+};
+
+const getDiff = (path1, path2, format = 'stylish') => {
+  const [data1, data2] = readFiles(path1, path2);
+
+  const parsedData1 = parse(data1, path.extname(path1).slice(1));
+  const parsedData2 = parse(data2, path.extname(path2).slice(1));
+
+  const difference = compare(parsedData1, parsedData2);
+  const formattedDiff = ast(difference, format);
+
+  return formattedDiff;
+};
 
 export default getDiff;
